a9addf0780c996fa4e9ac1a80adb2710263c06a8
Angelo Gonçalves da Luz <angelogl@gmail.com>
2020-04-10 09:04:55 -0300

    :sparkles: Add route post to add repositorie

--- src/app.js
+++ src/app.js
@@ -15,7 +15,16 @@ app.get("/repositories", (request, response) => {
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+    const {title, url, techs} = request.body;
+
+    const repositorie = {
+      id: uuid(),
+      title,
+      url,
+      techs,
+      likes: 0
+    }
+    repositories.push(repositorie);
 });
 
 app.put("/repositories/:id", (request, response) => {
